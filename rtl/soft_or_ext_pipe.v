19fc6b5debfadfe6a30afe86d401f078056cd24c
Patrick Allison <allison.122@osu.edu>
2014-07-27 12:37:28 -0400

    add missings

---
+++ rtl/soft_or_ext_pipe.v
@@ -0,0 +1,26 @@
+module soft_or_ext_pipe(input soft_i,
+							input ext_i,
+							input disable_ext_i,
+							input clk250_i,
+							input clk33_i,
+							output trig_o);
+	
+	reg soft_or_ext_clk33 = 0;
+
+	(* SHREG_EXTRACT = "NO" *)
+	reg [2:0] soft_or_ext_clk250 = 0;
+	
+	reg trig = 0;
+	
+	always @(posedge clk33_i) begin
+		soft_or_ext_clk33 <= soft_i || (ext_i && !disable_ext_i);
+	end
+	always @(posedge clk250_i) begin
+		soft_or_ext_clk250 <= {soft_or_ext_clk250[1:0],soft_or_ext_clk33};
+		trig <= soft_or_ext_clk250[2] && !soft_or_ext_clk250[1];
+	end
+
+	assign trig_o = trig;
+endmodule
+	
+							
